40b639a3016f4b2066223609c2f40208fee95d52
db-via <davis.birzgalis@va.lv>
2026-09-11 16:04:40 +0300

    adding section for retrieving references to html elements in script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+// HTML Elements
+const kerbolButton = document.getElementById("kerbol"); // The Kerbol (Sun) button
+const planetContainer = document.getElementById("planet-container"); // The container for the planet buttons
+const starField = document.getElementById("star-field"); // The star field background
+
+const infoPanel = document.getElementById("info-panel"); // The information panel that displays details about the selected celestial body
+const infoClose = document.getElementById("info-close"); // The close button for the information panel
+
+const infoImage = document.getElementById("info-image"); // The image element in the information panel
+const infoName = document.getElementById("info-name"); // The name element in the information panel
+const infoType = document.getElementById("info-type"); // The type element in the information panel
+const infoDescription = document.getElementById("info-description"); // The description element in the information panel
+const infoStatistics = document.getElementById("info-statistics"); // The statistics element in the information panel
+
+const ambientPlayer = document.getElementById("ambient-player"); // The audio player for ambient sounds
+const ambientName = document.getElementById("ambient-name"); // The name element for the ambient sound
